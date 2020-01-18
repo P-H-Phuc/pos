224a9c6643728dee2e5f31b6746170a967ed0d00
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-18 11:12:46 +0100

    [MIG][12.0] pos_payment_terminal
    Update pos_payment_terminal/static/src/js/devices.js
    
    Co-Authored-By: Iván Todorovich <ivan.todorovich@druidoo.io>
    
    [REF] rename field
    
    [UPD] Update pos_payment_terminal.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: pos-12.0/pos-12.0-pos_payment_terminal
    Translate-URL: https://translation.odoo-community.org/projects/pos-12-0/pos-12-0-pos_payment_terminal/
    
    [UPD] Update pos_payment_terminal.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: pos-12.0/pos-12.0-pos_payment_terminal
    Translate-URL: https://translation.odoo-community.org/projects/pos-12-0/pos-12-0-pos_payment_terminal/
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: pos-12.0/pos-12.0-pos_payment_terminal
    Translate-URL: https://translation.odoo-community.org/projects/pos-12-0/pos-12-0-pos_payment_terminal/es/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: pos-12.0/pos-12.0-pos_payment_terminal
    Translate-URL: https://translation.odoo-community.org/projects/pos-12-0/pos-12-0-pos_payment_terminal/

--- pos_payment_terminal/models/account_journal.py
+++ pos_payment_terminal/models/account_journal.py
@@ -2,13 +2,13 @@
 # © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 from odoo import models, fields
 
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
-    payment_mode = fields.Selection(
-        [('card', 'Card'), ('check', 'Check')], 'Payment Mode',
+    pos_terminal_payment_mode = fields.Selection(
+        selection=[('card', 'Card'), ('check', 'Check')],
+        string='Terminal Payment Mode',
         help="Select the payment mode sent to the payment terminal")
