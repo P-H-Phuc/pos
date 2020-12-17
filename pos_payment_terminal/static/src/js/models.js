066400863def581f34c6c28a28993b5b53d8e7d7
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-17 10:08:18 +0100

    pos_payment_terminal: black, isort and pre-commit stuff
    pos_payment_terminal: update README and screenshots
    
    pos_payment_terminal: remove commented lines
    
    pos_payment_terminal: fix lint issues

--- pos_payment_terminal/static/src/js/models.js
+++ pos_payment_terminal/static/src/js/models.js
@@ -7,11 +7,11 @@
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 */
 
-odoo.define('pos_payment_terminal.models', function (require) {
+odoo.define("pos_payment_terminal.models", function (require) {
+    "use strict";
 
-    var models = require('point_of_sale.models');
-    var OCAPaymentTerminal = require('pos_payment_terminal.payment');
-    models.register_payment_method('oca_payment_terminal', OCAPaymentTerminal);
-    models.load_fields('pos.payment.method', 'oca_payment_terminal_mode');
-
+    var models = require("point_of_sale.models");
+    var OCAPaymentTerminal = require("pos_payment_terminal.payment");
+    models.register_payment_method("oca_payment_terminal", OCAPaymentTerminal);
+    models.load_fields("pos.payment.method", "oca_payment_terminal_mode");
 });
