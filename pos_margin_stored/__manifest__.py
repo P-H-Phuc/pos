0a15eda86a1b9aa83db864bbdbc36302590bb41d
Quentin Dupont <perso@quentindupont.fr>
2025-10-09 09:59:23 +0200

    Update pos_margin_stored/__manifest__.py
    Co-authored-by: Sylvain LE GAL <sylvain.legal@grap.coop>

--- pos_margin_stored/__manifest__.py
+++ pos_margin_stored/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "PoS Order Margin",
+    "name": "PoS Order Margin Stored",
     "summary": "Margin Stored on PoS Order and Pos Order Line",
     "version": "16.0.1.0.0",
     "category": "Point Of Sale",
