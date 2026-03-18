6d834e73256717cc89e6a2810309389fb06cbd92
cristina-hidalgo-tecnativa <cristina.hidalgo@tecnativa.com>
2026-03-18 10:52:08 +0100

    [MIG] pos_customer_tree_view_vat: Migration to 18.0

--- pos_customer_tree_view_vat/__manifest__.py
+++ pos_customer_tree_view_vat/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Pos Vat Tree",
     "summary": """Point of Sale: Show VAT number at Customer Tree View""",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
@@ -13,17 +13,17 @@
         "point_of_sale",
     ],
     "assets": {
-        "point_of_sale.assets": [
+        "point_of_sale._assets_pos": [
             (
                 "after",
-                "point_of_sale/static/src/xml/Screens/PartnerListScreen/PartnerLine.xml",
+                "point_of_sale/static/src/app/screens/partner_list/partner_line/partner_line.xml",
                 "pos_customer_tree_view_vat/static/src/xml/partner_line.xml",
             ),
             (
                 "after",
-                "point_of_sale/static/src/xml/Screens/PartnerListScreen/PartnerListScreen.xml",
+                "point_of_sale/static/src/app/screens/partner_list/partner_list.xml",
                 "pos_customer_tree_view_vat/static/src/xml/partner_list_screen.xml",
             ),
-        ]
+        ],
     },
 }
